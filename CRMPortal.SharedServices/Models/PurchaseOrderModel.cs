3a41620607c5f8b7f0ceec471d47ed911dcb09fa
DawoudMina <minadawoudshahhat@gmail.com>
2017-06-06 10:08:44 +0200

    Login Edit
    Mina

--- CRMPortal.SharedServices/Models/PurchaseOrderModel.cs
+++ CRMPortal.SharedServices/Models/PurchaseOrderModel.cs
@@ -34,7 +34,7 @@ namespace CRMPortal.SharedServices.Models
         public List<Entity> GetAllRequests(Guid usr_id)
         {
 
-            return Context.CreateQuery("new_purchaserequest")/*.Where(r => r["new_relatedemployeeid"] == new EntityReference("systemuser", usr_id))*/.ToList();
+            return Context.CreateQuery("new_purchaserequest").Where(r => r["new_relatedemployeeid"] == new EntityReference("systemuser", usr_id)).ToList();
         }
 
     }
