e183599bb99b7cac33aaba14d36d674c05f23122
mohamedsaad306 <makcemos_X@yahoo.com>
2017-06-08 09:56:53 +0200

    HD Updates a

--- CRMPortal.SharedServices/Models/HelpDeskModel.cs
+++ CRMPortal.SharedServices/Models/HelpDeskModel.cs
@@ -13,35 +13,31 @@ namespace CRMPortal.SharedServices.Models
             : base(ctx)
         {
         }
-       
-        public Entity Entity
-        {
-            get
-            {
-                return Context.CreateQuery("systemuser").Where(u => u["internalemailaddress"] == AuthenticationCredintial.Username).FirstOrDefault();
-            }
-        }
-
-
-        public string Titel 
-        {
-            get
-            {
-                return Context.CreateQuery("systemuser").Where(u => u["internalemailaddress"] == AuthenticationCredintial.Username).FirstOrDefault()["lastname"].ToString();
-            }
-        }
-
         public List<Entity> GetAllRequests(Guid usr_id)
         {
 
            return  Context.CreateQuery("new_helpdeskrequest").Where(r => r["new_relatedemployeeid"] == new EntityReference("systemuser", usr_id)).ToList();
         }
-
-
         internal void SubmitRequest(Entity req)
         {
             Context.AddObject(req);
             Context.SaveChanges();
         }
+        //public Entity Entity
+        //{
+        //    get
+        //    {
+        //        return Context.CreateQuery("systemuser").Where(u => u["internalemailaddress"] == AuthenticationCredintial.Username).FirstOrDefault();
+        //    }
+        //}
+
+
+        //public string Titel 
+        //{
+        //    get
+        //    {
+        //        return Context.CreateQuery("systemuser").Where(u => u["internalemailaddress"] == AuthenticationCredintial.Username).FirstOrDefault()["lastname"].ToString();
+        //    }
+        //}
     }
 }
